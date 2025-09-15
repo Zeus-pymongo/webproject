94482b2b834990fd53ab9923ce7c8f948cd3907d
root <root@python.cloud.com>
2025-09-15 10:48:31 +0900

    first

---
+++ my-flask-app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 메인 페이지 라우팅
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# 서버 실행
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
