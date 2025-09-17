35e0eceac9a47c90e0192326216092f3286e0533
xinun <gnswlwjs777@hansung.ac.kr>
2025-09-17 19:26:59 +0900

    map readt

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")  # templates/index.html 렌더링
+    naver_client_id = os.getenv('NAVER_MAP_CLIENT_ID')
+    
+    # [★★★★★ 디버깅 코드 추가 ★★★★★]
+    # 터미널에 .env에서 읽은 값을 출력해봅니다.
+    print("---")
+    print(f"Flask가 .env에서 읽은 Client ID: '{naver_client_id}'")
+    print("---")
+    
+    return render_template('index.html', naver_client_id=naver_client_id)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
